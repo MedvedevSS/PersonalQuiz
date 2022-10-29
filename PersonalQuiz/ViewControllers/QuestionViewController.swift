8aee47e202f0acf02a2303d79888f52aafd0d396
devGod <devgod@mail.ru>
2022-10-29 23:45:31 +0300

    added UI added model

---
+++ PersonalQuiz/ViewControllers/QuestionViewController.swift
@@ -0,0 +1,12 @@
+//
+//  QuestionViewController.swift
+//  PersonalQuiz
+//
+//  Created by Sergey Medvedev on 29.10.2022.
+//
+
+import UIKit
+
+class QustionViewController: UIViewController {
+    
+}
